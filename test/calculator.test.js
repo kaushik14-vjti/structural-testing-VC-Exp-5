7d5a7e7975ff3803f51da95d062b3a81b6d590e8
kaushik <ksshukla_m22@ce.vjti.ac.in>
2023-02-02 10:29:59 +0530

    Structural Testing

---
+++ test/calculator.test.js
@@ -0,0 +1,64 @@
+// const calculator = require('./calculator')
+
+// const add = require('./add')
+
+// test ('properly adds to number', () =>{
+//     except(add(1,2)).toBe(3)
+// })
+
+
+
+// const sub = require('./sub')
+// const mul = require('./mul')
+// const div = require('./div')
+
+
+const calc = require('../src/calculator');
+
+const assert = require('assert');
+const describe = require('@jest/globals').describe;
+const it = require('@jest/globals').it;
+
+describe('Unit Testing', function () {
+  describe('add', function () {
+    it('should return sum of input', function () {
+      assert.equal(calc.add(5,10) ,15);
+    });
+  });
+  describe('sub', function () {
+    it('should return substracted value of input', function () {
+      assert.equal(calc.sub(10,5) ,5);
+    });
+  });
+  describe('mul', function () {
+    it('should return product of input', function () {
+      assert.equal(calc.mul(5,10) ,50);
+    });
+  });
+  describe('div', function () {
+    it('should return difference of input', function () {
+      assert.equal(calc.div(10,5) ,2);
+    });
+  });
+});
+
+describe('Functional Testing', function(){
+  describe('Calculator',function(){
+    it('+ operation', function(){
+      assert.equal(calc.calculator(5,10,'+'),15);
+    });
+    it('- operation', function(){
+      assert.equal(calc.calculator(10,5,'-'),5);
+    });
+    it('* operation', function(){
+      assert.equal(calc.calculator(5,10,'*'),50);
+    });
+    it('/ operation', function(){
+      assert.equal(calc.calculator(8,4,'/'),2);
+    });
+
+    it('Invalid operation', function(){
+      assert.equal(calc.calculator(8,4,'%'),'Wrong operator');
+    });
+  });
+});
